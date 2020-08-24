f8fa4cd707895480c6e5d43c5783e4df15484f2c
Mike Hernandez <hernandezmichael321@gmail.com>
2020-08-24 16:00:47 -0600

    Refactor num_chars method using compact and length

--- casidoo_challenges/num_chars.rb
+++ casidoo_challenges/num_chars.rb
@@ -3,15 +3,11 @@
 # => 2
 
 def num_chars(phrase, letter)
-  counter = 0
   split_phrase = phrase.split('')
   matches = split_phrase.map do |phrase_letter|
-    phrase_letter == letter
-  end
-  matches.each do |result|
-    counter += 1 if result == true
-  end
-  print counter
+    phrase_letter == letter ? true : nil
+  end.compact
+  print matches.length
 end
 
 
