ca356375d86017ced368fc775f80bb6fb65502ca
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-01 14:39:01 -0600

    Basic setup for stock_buy_sell test

---
+++ casidoo_challenges/stock_buy_sell.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+# Given an array of numbers that represent stock prices (where each number is
+# the price for a certain day), find 2 days when you should buy and sell your
+# stock for the highest profit.
+#
+# Example:
+#
+# stockBuySell([110, 180, 260, 40, 310, 535, 695])
+# “buy on day 4, sell on day 7”
+
+class StockBuySellTest < Minitest::Test
+end
