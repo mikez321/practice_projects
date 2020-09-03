997eb091c805017c65d63987d6b8695403a0f909
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-02 19:31:08 -0600

    Begin writing a recusive method to add nodes with comments left in for now

--- complete_me/lib/complete_me.rb
+++ complete_me/lib/complete_me.rb
@@ -1,16 +1,25 @@
 require './lib/node'
 
 class CompleteMe
-  attr_reader :root, :nodes
+  attr_reader :root
 
   def initialize
     @root = Node.new(" ")
-    @nodes = []
   end
 
-  def insert(word)
-    new_node = Node.new(word)
-    @nodes << new_node
+  def insert(word, current_node = @root)
+    #this is almost right, but I need to be naming the new node 'ab' on
+    #round 2 and then removing 'ab' from the next new word that gets passed in
+    
+    require "pry"; binding.pry
+    word = (current_node.name + word).delete(' ')
+    split_word = word.split('')
+    new_node = Node.new(split_word.first)
+    current_node.add(new_node)
+    split_word.shift
+    new_word = split_word.join
+    require "pry"; binding.pry
+    insert(new_word, new_node)
   end
 
   def count
