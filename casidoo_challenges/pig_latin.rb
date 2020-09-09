bc8c0d240501d97f5356c83700f08053c97c109a
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-09 10:43:17 -0600

    Create test for multiple words into pig latin

--- casidoo_challenges/pig_latin.rb
+++ casidoo_challenges/pig_latin.rb
@@ -14,5 +14,8 @@ class PigLatinTest < Minitest::Test
     assert_equal 'igpay', pig_latinize('pig')
     assert_equal 'atinlay', pig_latinize('latin')
   end
-  
+
+  def test_it_can_translate_multiple_words
+    assert_equal 'igpay atinlay', translate('pig latin')
+  end
 end
