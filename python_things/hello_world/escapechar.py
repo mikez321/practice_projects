c9d662b1338949fded28a16c92300a9da727ee44
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-11 20:05:57 -0600

    3 or so lessons on the escape character within strings

---
+++ python_things/hello_world/escapechar.py
@@ -0,0 +1,21 @@
+"""This is a Udemy lesson about the escape character."""
+splitString = "This string has been \nsplit over \nseveral \nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,... he\'s resting".')
+# or
+print("The pet shop owner said \"No, no 'e 's uh,... he's resting\".")
+print("""The pet shop owner said "No, no, 'e's uh,... he's resting".""")
+# this is some kinda fancy triple quote stuff!
+anotherSplitString = """This string has been \
+split over \
+several
+lines"""
+print(anotherSplitString)
+# and this is how you can print a \ in your code
+# print("C:\Users\timbuchalka\notes.txt")
+print("C:\\Users\\timbuchalka\\notes.txt")
+print(r"C:\Users\timbuchalka\notes.txt")
