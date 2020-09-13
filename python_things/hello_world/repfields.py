b8fcd5b2a6086425329715aa4eda4e385922769e
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-13 15:50:26 -0600

    Add example of f strings and injecting expression in {2} place

--- python_things/hello_world/repfields.py
+++ python_things/hello_world/repfields.py
@@ -6,3 +6,9 @@ print("My name is {0} years.".format(age))
 
 print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, and {7}."
       .format(31, "Jan", "March", "May", "July", "Aug", "Oct", "Dec"))
+
+print()
+num1 = 3
+num2 = 1
+print(f"The sum of {num1} and {num2} is {num1 + num2}.")
+print("The sum of {0} and {1} is {2}.".format(num1, num2, num1 + num2))
