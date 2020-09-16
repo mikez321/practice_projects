1f237a054c13d4898bfefe736af6b30fbd7636cf
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-16 13:07:58 -0600

    Add statement that shows 0 and empty strings evaluating to False

--- python_things/programflow/true_false.py
+++ python_things/programflow/true_false.py
@@ -1,11 +1,18 @@
 """Practice with Booleans."""
 
 
-day = "Monday"
-temperature = 30
-raining = True
-
-if day == "Saturday" and temperature > 27 and not raining:
-    print("Go Swimming")
+# day = "Monday"
+# temperature = 30
+# raining = True
+#
+# if day == "Saturday" and temperature > 27 and not raining:
+#     print("Go Swimming")
+# else:
+#     print("Learn Python")
+name = print("Please enter your name: ")
+# I did this with name = int(input()) and if you enter
+# a 0 as the name, it does evaluate to false.  Neat!
+if name:
+    print("Hello {}".format(name))
 else:
-    print("Learn Python")
+    print("Totes False")
