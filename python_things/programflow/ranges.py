73c24cf021b150c6867bd264cba1d40ba11d24f2
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-16 14:29:11 -0600

    More info on ranges

--- python_things/programflow/ranges.py
+++ python_things/programflow/ranges.py
@@ -1,4 +1,12 @@
 """For loops with ranges."""
 
-for i in range(1, 21):
+# for i in range(1, 21):
+
+# if a range doesn't have a starting value
+# it will start at 0.  The ending works the same.
+# for i in range(21):
+
+# if you want to change the step, just add it
+# but you'll need to specify a starting point
+for i in range(0, 21, 2):
     print("i is now {}".format(i))
