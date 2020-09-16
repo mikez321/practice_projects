081a13155379e580f18271ff63d781318908fe6a
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-16 12:56:30 -0600

    Practice with true and false

---
+++ python_things/programflow/true_false.py
@@ -0,0 +1,11 @@
+"""Practice with Booleans."""
+
+
+day = "Monday"
+temperature = 30
+raining = True
+
+if day == "Saturday" and temperature > 27 and not raining:
+    print("Go Swimming")
+else:
+    print("Learn Python")
