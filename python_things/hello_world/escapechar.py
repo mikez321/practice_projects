f84abfd9943e6e7ebbc01eb6bb352fa53cc50e41
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-17 17:37:33 -0600

    Change camel case var names to snake case

--- python_things/hello_world/escapechar.py
+++ python_things/hello_world/escapechar.py
@@ -1,20 +1,20 @@
 """This is a Udemy lesson about the escape character."""
-splitString = "This string has been \nsplit over \nseveral \nlines"
-print(splitString)
+split_string = "This string has been \nsplit over \nseveral \nlines"
+print(split_string)
 
-tabbedString = "1\t2\t3\t4\t5"
-print(tabbedString)
+tabbed_string = "1\t2\t3\t4\t5"
+print(tabbed_string)
 
 print('The pet shop owner said "No, no, \'e\'s uh,... he\'s resting".')
 # or
 print("The pet shop owner said \"No, no 'e 's uh,... he's resting\".")
 print("""The pet shop owner said "No, no, 'e's uh,... he's resting".""")
 # this is some kinda fancy triple quote stuff!
-anotherSplitString = """This string has been \
+another_split_string = """This string has been \
 split over \
 several
 lines"""
-print(anotherSplitString)
+print(another_split_string)
 # and this is how you can print a \ in your code
 # print("C:\Users\timbuchalka\notes.txt")
 print("C:\\Users\\timbuchalka\\notes.txt")
