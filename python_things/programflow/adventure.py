175e3e2ff5757ff692841977303322c0c6c8344e
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-17 13:15:43 -0600

    Write a while loop

---
+++ python_things/programflow/adventure.py
@@ -0,0 +1,13 @@
+"""Practice with While Loops."""
+
+available_exits = ['north', 'south', 'east', 'west']
+
+chosen_exit = ''
+
+while chosen_exit not in available_exits:
+    chosen_exit = input('Choose a direction to exit: ')
+    if chosen_exit.casefold() == 'quit':
+        print('Game over')
+        break
+
+print('Yay, you escaped the room!')
