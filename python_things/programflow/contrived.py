f6a6ec3165320a7a7fd35f4b5eb3316c04effd34
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-17 18:01:42 -0600

    Contrived example of using an else in a for loop

---
+++ python_things/programflow/contrived.py
@@ -0,0 +1,10 @@
+"""This is an example of using else in a loop."""
+numbers = [1, 45, 31, 16, 60]
+
+for number in numbers:
+    if number % 8 == 0:
+        # reject the list
+        print("The numbers are unacceptable")
+        break
+else:
+    print("All those numbers are fine")
