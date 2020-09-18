dae27dae5ee99e21e3d525fc9be5fa9c877b13c9
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-17 18:02:07 -0600

    Add else in for loop for binary guess game

--- python_things/programflow/hilow.py
+++ python_things/programflow/hilow.py
@@ -8,7 +8,7 @@ input("Press ENTER to start")
 
 count = 1
 
-while True:
+while low != high:
     guess = low + (high - low) // 2
     # the above formula was presented in the slides of the previous lesson
     # and calculates the midpoint of two numbers in a binary search
@@ -32,3 +32,9 @@ while True:
         break
     else:
         print("Please enter h, l, or c")
+# here's a more useful and impressive use of an else in a for loop
+# if the computer knows your number it can just spit it out immediately and
+# this line will execute!
+else:
+    print("Your number is {}!".format(low))
+    print("I got it in {} guesses!".format(count))
