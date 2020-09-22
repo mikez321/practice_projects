cc109508074aaca28864fd058f20c102ecb1e7fa
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-22 11:17:50 -0600

    Review of how lists work like strings with slice

---
+++ python_things/lists/lists_intro.py
@@ -0,0 +1,14 @@
+"""List lesson from Python Udemy course."""
+computer_parts = ['computer',
+                  'monitor',
+                  'keyboard',
+                  'mouse',
+                  'mouse mat'
+                  ]
+
+for part in computer_parts:
+    print(part)
+
+print(computer_parts[2])
+print(computer_parts[0:3])
+print(computer_parts[-1])
