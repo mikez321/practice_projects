f6375c6317066c4a45cca19fa7bd84bd5580493c
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-23 14:17:11 -0600

    Replace items in computer parts list using slices

--- python_things/lists/lists_intro.py
+++ python_things/lists/lists_intro.py
@@ -5,10 +5,14 @@ computer_parts = ['computer',
                   'mouse',
                   'mouse mat'
                   ]
-
-for part in computer_parts:
-    print(part)
-
-print(computer_parts[2])
-print(computer_parts[0:3])
-print(computer_parts[-1])
+#
+# for part in computer_parts:
+#     print(part)
+#
+# print(computer_parts[2])
+# print(computer_parts[0:3])
+# print(computer_parts[-1])
+print(computer_parts)
+# computer_parts[3] = 'trackball'
+computer_parts[3:] = ['trackball', 'lamp', 'speakers', 'microphone']
+print(computer_parts)
