62e0a7ab933f67dba57f6c8f685f4b9cb36e7f32
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-23 12:24:51 -0600

    Introduce basic sequence methods

---
+++ python_things/lists/number_lists.py
@@ -0,0 +1,13 @@
+"""Intro to some basic sequence methods."""
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+print(min(even))
+print(max(even))
+print(min(odd))
+print(max(odd))
+print('*' * 20)
+print(len(even))
+print(len(odd))
+print('*' * 20)
+print('mississippi'.count("iss"))
