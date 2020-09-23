a8d12d339e01e2fc8501f7a22e6c77000658e6d3
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-23 13:13:44 -0600

    Complete flower shrub list challenge

---
+++ python_things/lists/flower_shrub.py
@@ -0,0 +1,36 @@
+"""Flower Shrub code challenge."""
+# Separate the single list into two.  One should contain flowers, and the other
+# should contain shrubs.
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac- Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flowers = []
+shrubs = []
+
+for plant in data:
+    if 'flower' in plant.casefold():
+        flowers.append(plant)
+    else:
+        shrubs.append(plant)
+
+print(flowers)
+print(shrubs)
