eee97a94451931e020196597307bbb25247fb45b
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-23 13:07:58 -0600

    Use lists to refactor code and make it scalable

--- python_things/lists/buy_computer.py
+++ python_things/lists/buy_computer.py
@@ -1,31 +1,29 @@
-"""Lesson."""
+"""Iterating over lists."""
+available_parts = ['computer',
+                   'monitor',
+                   'keyboard',
+                   'mouse',
+                   'hdmi cable',
+                   'dvd drive']
 current_choice = "-"
+# valid_choices = [str(i) for i in range(1, len(available_parts) + 1)]
+valid_choices = []
+for i in range(1, len(available_parts) + 1):
+    valid_choices.append(str(i))
+
+print(valid_choices)
 computer_parts = []  # create an empty list
 
 while current_choice != '0':
-    if current_choice in "123456":
+    if current_choice in valid_choices:
         print("Adding {}".format(current_choice))
-        if current_choice == '1':
-            computer_parts.append('computer')
-        elif current_choice == '2':
-            computer_parts.append('monitor')
-        elif current_choice == '3':
-            computer_parts.append('keyboard')
-        elif current_choice == '4':
-            computer_parts.append('mouse')
-        elif current_choice == '5':
-            computer_parts.append('mouse mat')
-        elif current_choice == '6':
-            computer_parts.append('hdmi cable')
+        index = int(current_choice) - 1
+        chosen_part = available_parts[index]
+        computer_parts.append(chosen_part)
     else:
         print("Please add options from below:")
-        print("1: computer")
-        print("2: monitor")
-        print("3: keyboard")
-        print("4: mouse")
-        print("5: mouse mat")
-        print("6: hdmi cable")
-        print("0: to finish")
+        for number, part in enumerate(available_parts):
+            print("{0}: {1}".format(number + 1, part))
 
     current_choice = input()
 
