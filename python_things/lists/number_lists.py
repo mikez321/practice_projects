e320eab953e7934b4c29cc831d972a2daba140c6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-23 14:09:27 -0600

    Copy lists in various ways

--- python_things/lists/number_lists.py
+++ python_things/lists/number_lists.py
@@ -1,6 +1,26 @@
 """Intro to some basic sequence methods."""
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
+empty_list = []
+
+numbers = even + odd
+
+print(numbers)
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+print(numbers)
+print(list('2198462'))
+more_numbers = list(numbers)
+print(more_numbers)
+print(id(numbers))
+print(id(more_numbers))
+print(numbers is more_numbers)
+even_more_numbers = numbers[:]
+print(even_more_numbers)
+print(even_more_numbers is numbers)
+too_many_numbers = numbers.copy()
+print(too_many_numbers)
+print(too_many_numbers is numbers)
 
 # print(min(even))
 # print(max(even))
@@ -11,9 +31,9 @@ odd = [1, 3, 5, 7, 9]
 # print(len(odd))
 # print('*' * 20)
 # print('mississippi'.count("iss"))
-even.extend(odd)
-print(even)
-even.sort()
-print(even)
-even.sort(reverse=True)
-print(even)
+# even.extend(odd)
+# print(even)
+# even.sort()
+# print(even)
+# even.sort(reverse=True)
+# print(even)
