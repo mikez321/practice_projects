845181d48696ad05e1e956b2c4bf97acbae2bd25
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-24 11:58:55 -0600

    Create string out of list with separator and join method

---
+++ python_things/lists/joining_things.py
@@ -0,0 +1,16 @@
+"""Join items in lists."""
+flowers = [
+    "Daffodil",
+    "Evening Primrose",
+    "Hydrangea",
+    "Iris",
+    "Lavender",
+    "Sunflower",
+    "Tiger Lily",
+]
+# for flower in flowers:
+#     print(flower)
+
+separator = ' | '
+
+print(separator.join(flowers))
