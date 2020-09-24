79dcdadd9e141be4d5fe747fd764fefca28f8ddc
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-24 13:57:49 -0600

    Complete mini code challenge

--- python_things/lists/splitting_things.py
+++ python_things/lists/splitting_things.py
@@ -11,3 +11,11 @@ digit_list = number.split(',')
 print(digit_list)
 rejoined_numbers = "".join(digit_list)
 print(rejoined_numbers)
+print('*' * 5 + ' Challenge Below! ' + '*' * 5)
+# splitting a string returns a list of strings. Use a for loop
+# and split that number into a list of ints.
+int_list = []
+for group in digit_list:
+    int_list.append(int(group))
+
+print(int_list)
