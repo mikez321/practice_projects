30379413d5de41c3e0b7bd605b5e8094bdc993fa
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-24 11:45:32 -0600

    Use extra args with print function

---
+++ python_things/lists/moreprint.py
@@ -0,0 +1,7 @@
+"""Explore more functionality of the print fucntion."""
+name = 'Mike'
+age = '36'
+
+print(name, age, "Python", 2020)
+print(name, age, "Python", 2020, sep=', ')
+print(name, age, "Python", 2020, sep="\n", end='DONE')
