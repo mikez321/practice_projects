e937b2854f89c808c497eccb5ab89a8221cf4e1e
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-24 14:54:20 -0600

    Revisit how enumerate function works by unpacking tuples

--- python_things/lists/enumerate_example.py
+++ python_things/lists/enumerate_example.py
@@ -1,3 +1,9 @@
 """Enumerate lesson."""
-for index, character in enumerate('abcdefgh'):
+# for index, character in enumerate('abcdefgh'):
+# print(index, character)
+for t in enumerate('abcdefgh'):
+    index, character = t
     print(index, character)
+# the code above is illustrating that the enumerate function
+# creates a tuple of the index and character and they can then
+# be accessed by unpacking said tuple into variables.
