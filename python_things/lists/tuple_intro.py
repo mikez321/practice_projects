2087a3dc112691b068815efc0fd5ca3b11343867
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-24 20:02:49 -0600

    More work with tuples

--- python_things/lists/tuple_intro.py
+++ python_things/lists/tuple_intro.py
@@ -2,21 +2,14 @@
 # t = 'a', 'b', 'c'
 #
 # print(t)
-welcome = ('Welcome to my Nightmare', 'Alice Cooper', 1975)
-bad = ('Bad Company', 'Bad Company', 1974)
-budgie = ('Nightflight', 'Budgie', 1981)
-imelda = ('More Mayhem', 'Emilda May', 2011)
-metallica = ('Ride the Lightning', 'Metallica', 1984)
+albums = [
+    ('Welcome to my Nightmare', 'Alice Cooper', 1975),
+    ('Bad Company', 'Bad Company', 1974),
+    ('Nightflight', 'Budgie', 1981),
+    ('More Mayhem', 'Emilda May', 2011),
+    ('Ride the Lightning', 'Metallica', 1984),
+]
 
-print(metallica)
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
-
-# The following code causes errors and won't run
-# metallica[2] = 1986
-# print(metallica)
-
-metallica2 = list(metallica)
-metallica2[2] = 1986
-print(metallica2)
+for album in albums:
+    title, artist, year = album
+    print(f"Album: {title}, Artist: {artist}, Year: {year}")
