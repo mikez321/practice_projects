adae194f23b4b8b68317e46eb6bf5a43b6456598
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-29 15:05:20 -0600

    Update functionality so jukebox starts over if invalid song is chosen

--- python_things/lists/jukebox_menu.py
+++ python_things/lists/jukebox_menu.py
@@ -5,24 +5,22 @@ SONGS_LIST_INDEX = 3
 SONG_TITLE_INDEX = 1
 
 while True:
+    print("=" * 15 + " Main Menu " + "=" * 15)
     print("Please choose your album (invalid choice will exit):")
     for index, (title, artist, year, songs) in enumerate(albums):
         print(f"{index + 1}: {title}")
-    choice = int(input())
-    if 1 <= choice <= len(albums):
-        songs_list = albums[choice - 1][SONGS_LIST_INDEX]
+    album_choice = int(input())
+    if 1 <= album_choice <= len(albums):
+        songs_list = albums[album_choice - 1][SONGS_LIST_INDEX]
     else:
         break
 
-    print("Please choose your song:")
+    print("Please choose your song (invalid choice returns to album menu):")
     for index, (track_number, song) in enumerate(songs_list):
         print(f"{index + 1}: {song}")
 
     song_choice = int(input())
     if 1 <= song_choice <= len(songs_list):
         title = songs_list[song_choice - 1][SONG_TITLE_INDEX]
-    else:
-        break
-
-    print(f"Playing {title}")
-    print("=" * 40)
+        print(f"Playing {title}")
+        print("=" * 41)
