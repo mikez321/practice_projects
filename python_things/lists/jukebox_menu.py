3f16b8109b663c354456d15220117970be4fd315
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-29 14:46:56 -0600

    Add to jukebox code

--- python_things/lists/jukebox_menu.py
+++ python_things/lists/jukebox_menu.py
@@ -1,8 +1,28 @@
 """Write a jukebox app."""
 from nested_data import albums
 
+SONGS_LIST_INDEX = 3
+SONG_TITLE_INDEX = 1
+
 while True:
     print("Please choose your album (invalid choice will exit):")
     for index, (title, artist, year, songs) in enumerate(albums):
-        print(f"{index + 1}: {title}, {artist}, {year}, {songs}")
-    break
+        print(f"{index + 1}: {title}")
+    choice = int(input())
+    if 1 <= choice <= len(albums):
+        songs_list = albums[choice - 1][SONGS_LIST_INDEX]
+    else:
+        break
+
+    print("Please choose your song:")
+    for index, (track_number, song) in enumerate(songs_list):
+        print(f"{index + 1}: {song}")
+
+    song_choice = int(input())
+    if 1 <= song_choice <= len(songs_list):
+        title = songs_list[song_choice - 1][SONG_TITLE_INDEX]
+    else:
+        break
+
+    print(f"Playing {title}")
+    print("=" * 40)
