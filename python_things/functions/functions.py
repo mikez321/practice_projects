59772f32b17ec6dad928c2ebaa0ac83baf267862
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-30 11:19:04 -0600

    Write a simple multiplication function

---
+++ python_things/functions/functions.py
@@ -0,0 +1,18 @@
+"""Work with functions in Python."""
+
+
+def multiply(x, y):
+    """Multiply two numbers, x and y."""
+    result = x * y
+    return result
+
+
+answer = multiply(10.5, 4)
+print(answer)
+
+forty_two = multiply(6, 7)
+print(forty_two)
+
+for val in range(1, 5):
+    two_times = multiply(2, val)
+    print(two_times)
