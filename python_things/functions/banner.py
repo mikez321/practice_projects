a03fb685d1347c44ea955998bff4e05a50b139e0
Mike Hernandez <hernandezmichael321@gmail.com>
2020-09-30 12:54:24 -0600

    Update banner width function to accept an arg for banner width

--- python_things/functions/banner.py
+++ python_things/functions/banner.py
@@ -1,9 +1,8 @@
 """Create a banner across the screen."""
 
 
-def banner_text(text):
+def banner_text(text, screen_width):
     """Create a banner with given text."""
-    screen_width = 50
     if len(text) > screen_width - 4:
         raise ValueError(f"String '{text}' is larger than specified "
                          f"width {screen_width}.")
@@ -16,14 +15,14 @@ def banner_text(text):
         print(output_string)
 
 
-banner_text("*")
-banner_text("Always look on the bright side of life...")
-banner_text("If life seems jolly rotten,")
-banner_text("There's something you've forgotten!")
-banner_text("And that's to laugh and smile and dance and sing,")
-banner_text(" ")
-banner_text("When you're feeling in the dumps,")
-banner_text("Don't be silly chumps,")
-banner_text("Just purse your lips and whistle - that's the thing!")
-banner_text("And... always look on the bright side of life...")
-banner_text("*")
+banner_text("*", 80)
+banner_text("Always look on the bright side of life...", 80)
+banner_text("If life seems jolly rotten,", 80)
+banner_text("There's something you've forgotten!", 80)
+banner_text("And that's to laugh and smile and dance and sing,", 80)
+banner_text(" ", 80)
+banner_text("When you're feeling in the dumps,", 80)
+banner_text("Don't be silly chumps,", 80)
+banner_text("Just purse your lips and whistle - that's the thing!", 80)
+banner_text("And... always look on the bright side of life...", 80)
+banner_text("*", 80)
