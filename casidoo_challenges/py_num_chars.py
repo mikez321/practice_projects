08ddacb9503c47d3acfa6293ead4255f7faebb74
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-02 16:10:55 -0600

    Import and implement unittest for challenge

--- casidoo_challenges/py_num_chars.py
+++ casidoo_challenges/py_num_chars.py
@@ -1,4 +1,5 @@
 """Casidoo challenge for number of characters."""
+import unittest
 # Given a string s and a character c, return the number of occurrences
 # of c in s.
 # Ex: num_chars('oh heavens', 'h')
@@ -14,5 +15,15 @@ def num_chars(phrase, letter):
     return counter
 
 
-print(num_chars('oh heavens', 'h') == 2)
-print(num_chars('she sells seashells down by the seashore', 's') == 8)
+class NumCharsTest(unittest.TestCase):
+    """Testing for num_chars function."""
+
+    def test_it_knows_how_many_given_characters_are_in_a_string(self):
+        """Return how many times a letter appears in a string."""
+        self.assertEqual(num_chars('oh_heavens', 'h'), 2)
+        self.assertEqual(num_chars('she sells seashells down by the seashore',
+                                   's'), 8)
+
+
+if __name__ == '__main__':
+    unittest.main()
