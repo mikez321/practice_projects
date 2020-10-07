cad64f861278988a813e206f9a5bfcd8314c38e2
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-07 13:53:40 -0600

    Get basic functionaliy working

--- other_challenges/python/questionmarks.py
+++ other_challenges/python/questionmarks.py
@@ -17,7 +17,33 @@ def question_marks(code: str) -> bool:
         are 3 question marks between 6 and 4, and also three question marks
         between 5 and 5.
     """
-    pass
+    num_1 = None
+    index_1 = None
+    num_2 = None
+    index_2 = None
+    for index, char in enumerate(code):
+        if char.isnumeric() and num_1 is None:
+            num_1 = int(char)
+            index_1 = index
+        elif char.isnumeric():
+            num_2 = int(char)
+            index_2 = index
+
+            if num_1 + num_2 == 10:
+                snippet = code[index_1:(index_2 + 1)]
+                if snippet.count('?') != 3:
+                    return False
+                    break
+                else:
+                    num_1 = num_2
+                    index_1 = index_2
+                    num_2 = None
+                    index_2 = None
+
+    if num_1 is None:
+        return False
+    else:
+        return True
 
 
 class QuestionMarkTest(unittest.TestCase):
@@ -25,7 +51,8 @@ class QuestionMarkTest(unittest.TestCase):
 
     def test_it_passes_the_pattern(self):
         """It can identify if the passed in string adheres to the pattern."""
-        self.assertTrue(question_marks("acc?7??sss?3rr1??????5"))
+        self.assertEqual(question_marks("acc?7??sss?3rr1??????5"), True)
+        self.assertEqual(question_marks("aa6?9"), False)
 
 
 if __name__ == '__main__':
