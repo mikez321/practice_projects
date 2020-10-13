c4351f7916a0d28ab5f3addcc520102290ba6aa6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-12 18:32:56 -0600

    Add subtract functionality

--- casidoo_challenges/baby_lisp.rb
+++ casidoo_challenges/baby_lisp.rb
@@ -6,11 +6,20 @@ def baby_lisp(command)
   num1 = command.delete('()').split[-2].to_i
   num2 = command.delete('()').split[-1].to_i
 
-  num1 + num2
+  if action == 'add'
+    num1 + num2
+  elsif action == 'subtract'
+    num1 - num2
+  end
+
 end
 
 class BabyLispTest < Minitest::Test
   def test_it_can_add
     assert_equal baby_lisp('(add 3 2)'), 5
+  end
+
+  def test_it_can_subtract
+    assert_equal baby_lisp('(subtract 3 2)'), 1
   end
 end
