5895933c3d32a57eaa0420543fd24b191b245839
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-12 18:52:33 -0600

    Remove if/else and condense into hash for scalability

--- casidoo_challenges/baby_lisp.rb
+++ casidoo_challenges/baby_lisp.rb
@@ -2,38 +2,38 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 def baby_lisp(command)
-  action = command.delete('()').split.first
-  num1 = command.delete('()').split[-2].to_i
+  math = {
+    'add': :+,
+    'subtract': :-,
+    'multiply': :*,
+    'divide': :/
+  }
+
+  action = command.delete('()').split.first.to_sym
+  num1 = command.delete('()').split[-2].to_f
   num2 = command.delete('()').split[-1].to_i
 
-  if action == 'add'
-    num1 + num2
-  elsif action == 'subtract'
-    num1 - num2
-  elsif action == 'multiply'
-    num1 * num2
-  elsif action == 'divide'
-    num1.to_f / num2
-  else
-    "I don't know how to {action}"
-  end
-
+  num1.send(math[action], num2)
 end
 
 class BabyLispTest < Minitest::Test
   def test_it_can_add
-    assert_equal baby_lisp('(add 3 2)'), 5
+    assert_equal baby_lisp('(add 3 2)'), 5.0
   end
 
   def test_it_can_subtract
-    assert_equal baby_lisp('(subtract 3 2)'), 1
+    assert_equal baby_lisp('(subtract 3 2)'), 1.0
   end
 
   def test_it_can_multiply
-    assert_equal baby_lisp('(multiply 3 2)'), 6
+    assert_equal baby_lisp('(multiply 3 2)'), 6.0
   end
 
   def test_it_can_divide
     assert_equal baby_lisp('(divide 3 2)'), 1.5
+  end
+
+  def test_it_can_perform_more_than_one_calculation
+    assert_equal baby_lisp('(subtract 1 (add 3 2))'), 5.0
   end
 end
