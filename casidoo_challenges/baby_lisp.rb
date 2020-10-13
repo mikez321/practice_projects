3c52cc5969579b0cc31c565c562b5ff4255f6518
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-12 18:35:42 -0600

    Pass all basic math functions

--- casidoo_challenges/baby_lisp.rb
+++ casidoo_challenges/baby_lisp.rb
@@ -10,6 +10,12 @@ def baby_lisp(command)
     num1 + num2
   elsif action == 'subtract'
     num1 - num2
+  elsif action == 'multiply'
+    num1 * num2
+  elsif action == 'divide'
+    num1.to_f / num2
+  else
+    "I don't know how to {action}"
   end
 
 end
@@ -21,5 +27,13 @@ class BabyLispTest < Minitest::Test
 
   def test_it_can_subtract
     assert_equal baby_lisp('(subtract 3 2)'), 1
+  end
+
+  def test_it_can_multiply
+    assert_equal baby_lisp('(multiply 3 2)'), 6
+  end
+
+  def test_it_can_divide
+    assert_equal baby_lisp('(divide 3 2)'), 1.5
   end
 end
