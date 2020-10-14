e3c566a5e046bdeeeaa30b240610099b9939513c
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-14 12:10:52 -0600

    Implement testing

--- other_challenges/intersection.rb
+++ other_challenges/intersection.rb
@@ -1,9 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
 def find_intersection(strArr)
   result = []
-  str1 = strArr.first.split
-  str2 = strArr.last.split
-
-  all = str1 + str2
+  all = strArr.first.split + strArr.last.split
   all = all.map { |item| item.delete(',')}
   all.each do |item|
     if all.count(item) > 1
@@ -18,5 +18,8 @@ def find_intersection(strArr)
   end
 end
 
-
-puts find_intersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"])
+class FindIntersectionTest < Minitest::Test
+  def test_it_finds_the_numbers_in_both_strings
+    assert_equal find_intersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]), '1,4,13'
+  end
+end
