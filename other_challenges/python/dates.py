ca5e858a4785d5e48929642305f194526ced8a0f
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-19 14:37:39 -0600

    Set up unittest for code challenge

---
+++ other_challenges/python/dates.py
@@ -0,0 +1,14 @@
+"""Return a count of the frequency of dates."""
+import unittest
+
+
+class DateTest(unittest.TestCase):
+    """Testing for dates."""
+
+    def test_it_returns_a_frequency_of_dates(self):
+        """Return a number representning the frequency of date input."""
+        assertEqual(0, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
