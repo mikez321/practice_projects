7850932be52e745e9755bb4f3e8947cde82d1d61
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-19 14:50:02 -0600

    Begin writing function and add docstrings

--- other_challenges/python/dates.py
+++ other_challenges/python/dates.py
@@ -2,12 +2,56 @@
 import unittest
 
 
+def frequency(all_dates: list,
+              day: str = None,
+              month: str = None,
+              year: str = None) -> int:
+    """
+    Return the frequency a day month or year occurrs in the given dates.
+
+    :param all_dates: This is a list of all the possible dates to search
+        through.
+    :param day: A string representing the day of a month
+    :param month: A string representing the number of a month
+    :param year: A string representing the number of the year
+    :return: The return is an integer representing the frequency of how often
+        that day, month, or year occurrs in the provided dates.  It is possible
+        to pass in more than one parameter at a time.
+
+    :Examples:
+        dates = ["3/21/2004", "3/19/2004", "3/21/2020"]
+        frequency(dates, month='3')
+        >>> 3
+        frequency(dates, day='21')
+        >>> 2
+        frequency(dates, month='3', day='21')
+        >>> 2
+        frequency(dates, month='12')
+        >>> 0
+    """
+    
+
+
 class DateTest(unittest.TestCase):
     """Testing for dates."""
 
     def test_it_returns_a_frequency_of_dates(self):
         """Return a number representning the frequency of date input."""
-        assertEqual(0, 0)
+        dates = [
+            "11/14/2019",
+            "12/26/2019",
+            "1/1/2020",
+            "1/19/2020",
+            "2/11/2020",
+            "2/15/2020",
+            "3/11/2020",
+            "4/5/2020",
+            "4/11/2020",
+            "4/14/2020",
+            "4/26/2020",
+        ]
+
+        self.assertEqual(frequency(dates, month=3), 1)
 
 
 if __name__ == '__main__':
