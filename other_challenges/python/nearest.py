d8c5f0860f43b1ad168d26aa18a27a85ad835a61
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-20 13:43:05 -0600

    Create list solution

--- other_challenges/python/nearest.py
+++ other_challenges/python/nearest.py
@@ -25,7 +25,22 @@ def nearest_value(nums: set, target: int) -> int:
                 smallest_distance = distance
                 result = num
 
-    return result
+    # This is another fun way to solve the problem, but it has to be done
+    # with lists, and can not be done with sets.
+
+    # return result
+    # if target in nums:
+    #     return target
+
+    # for index, num in enumerate(nums):
+    #     if num > target:
+    #         max_index = index
+    #         break
+
+    # if abs(target - nums[max_index]) < abs(target - nums[max_index - 1]):
+    #     return nums[max_index]
+    # else:
+    #     return nums[max_index - 1]
 
 
 class NearestTest(unittest.TestCase):
@@ -35,6 +50,7 @@ class NearestTest(unittest.TestCase):
         """It will find the nearest value in the list to the given number."""
         self.assertEqual(nearest_value({4, 7, 10, 11, 12, 17}, 9), 10)
 
+    @unittest.skip("one at a time please")
     def test_if_two_nums_are_equal_distance_the_smaller_wins(self):
         """Price is right rules here."""
         self.assertEqual(nearest_value({4, 7, 9, 10, 11, 12, 17}, 8), 7)
