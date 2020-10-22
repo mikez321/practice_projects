6cbe3fcb4a9b99d19b6d4e9f8e86e903369c4b5a
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-21 18:27:24 -0600

    Pass challenge

--- other_challenges/python/btn_markers.py
+++ other_challenges/python/btn_markers.py
@@ -1,4 +1,17 @@
+"""Between markers lite challenge from py.checkio.org."""
 import unittest
+
+
+def between_markers(input: str, mk1: str, mk2: str) -> str:
+    """
+    Return the string inside the markers.
+
+    :param input: Any string which shoudl include markers.
+    :param mk1: This is the first marker and should be included in 'input.'
+    :param mk2: This is the 2nd marker and should also be included in 'input.'
+    :return: The return is whatever is encased in the markers.
+    """
+    return input[input.index(mk1) + 1:input.index(mk2)]
 
 
 class BetweenMarkersTest(unittest.TestCase):
