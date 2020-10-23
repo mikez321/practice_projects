7de3844fad60a5702b3ac2b7584b406e22b1c1f3
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-23 15:38:45 -0600

    Create vampire object

--- other_challenges/python/mythical_creatures/vampire.py
+++ other_challenges/python/mythical_creatures/vampire.py
@@ -1,1 +1,22 @@
 """Vampire object from Turing's Mythical Creatures."""
+
+
+class Vampire:
+    """Vampire class that wants to suck your blood."""
+
+    def __init__(self, name, pet='bat'):
+        """Initialize a vampire objet with a name and a pet bat."""
+        self.name = name
+        self.pet = pet
+        self.thirst = 0
+
+    def is_thirsty(self):
+        """Return boolean of thirsty or not."""
+        if self.thirst == 0:
+            return True
+        else:
+            return False
+
+    def drink(self):
+        """Change default thirst status."""
+        self.thirst = 1
