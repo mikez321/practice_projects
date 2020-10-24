ffda8036993ccc2fa368951e08551e783ef934a3
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-24 08:57:14 -0600

    Add name and location propperties as well as setters for each

--- other_challenges/python/mythical_creatures/tests/test_werewolf.py
+++ other_challenges/python/mythical_creatures/tests/test_werewolf.py
@@ -4,5 +4,14 @@ from werewolf import Werewolf
 
 def test_it_exists():
     """Test that a werewolf object is created."""
-    werewolf = Werewolf('David', 'London')
+    werewolf = Werewolf()
     assert type(werewolf) == Werewolf
+
+
+def test_it_has_attributes():
+    """Test that a werewolf has a name and location."""
+    werewolf = Werewolf()
+    werewolf.name = 'David'
+    werewolf.location = 'London'
+    assert werewolf.name == 'David'
+    assert werewolf.location == 'London'
--- other_challenges/python/mythical_creatures/werewolf.py
+++ other_challenges/python/mythical_creatures/werewolf.py
@@ -4,7 +4,25 @@
 class Werewolf:
     """Werewolf object."""
 
-    def __init__(self, name, location):
-        """Initialize new Werewolf object with name and location."""
-        self.name = name
-        self.location = location
+    def __init__(self):
+        """Initialize Werewolf object with name and location as properties."""
+        self.name = None
+        self.location = None
+
+    @property
+    def name(self):
+        """Getter method to return name."""
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def location(self):
+        """Getter method to return location."""
+        return self._location
+
+    @location.setter
+    def location(self, value):
+        self._location = value
