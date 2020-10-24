91e73a0b2f1004d405aaae69d55e5f2aca49fac6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-23 21:00:19 -0600

    Complete MC Dragon

--- other_challenges/python/mythical_creatures/dragon.py
+++ other_challenges/python/mythical_creatures/dragon.py
@@ -9,15 +9,27 @@ class Dragon():
         self.name = name
         self.color = color
         self.rider = rider
+        self.eat_count = 0
 
-    def get_name(self):
+    def get_name(self) -> str:
         """Getter method for returning dragon's name."""
         return self.name
 
-    def get_color(self):
+    def get_color(self) -> str:
         """Getter method for returning dragon's color."""
         return self.color
 
-    def get_rider(self):
+    def get_rider(self) -> str:
         """Getter method for returning dragon's rider name."""
         return self.rider
+
+    def is_hungry(self) -> bool:
+        """Return if dragon is hungry or not."""
+        if self.eat_count <= 3:
+            return True
+        else:
+            return False
+
+    def eat(self):
+        """Add to current eat count."""
+        self.eat_count += 1
