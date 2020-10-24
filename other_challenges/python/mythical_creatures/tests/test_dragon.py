6b42670b99029b942ea959f1b81db884166862c1
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-23 20:49:26 -0600

    Add more tests

--- other_challenges/python/mythical_creatures/tests/test_dragon.py
+++ other_challenges/python/mythical_creatures/tests/test_dragon.py
@@ -2,7 +2,20 @@
 from dragon import Dragon
 
 
-def test_it_has_a_name():
+def test_it_exists():
+    """Test that the object is created."""
+    dan = Dragon('Dan', 'Gold', 'James')
+    assert type(dan) == Dragon
+
+
+def test_it_has_a_name_and_color():
     """Test that a dragon has a name."""
-    tom = Dragon('Tom')
-    assert tom.name == 'Tom'
+    tom = Dragon('Tom', 'Gold', 'Ben')
+    assert tom.get_name() == 'Tom'
+    assert tom.get_color() == 'Gold'
+
+
+def test_it_has_a_rider():
+    """A dragon has a rider with a name."""
+    elise = Dragon('Elise', 'Green', 'Lessa')
+    assert elise.get_rider() == 'Lessa'
