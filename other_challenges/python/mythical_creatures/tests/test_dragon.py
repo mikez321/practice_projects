62532de4a2dcb17efca9d96761e2622aed69c63e
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-23 21:00:11 -0600

    Add more tests to dragon

--- other_challenges/python/mythical_creatures/tests/test_dragon.py
+++ other_challenges/python/mythical_creatures/tests/test_dragon.py
@@ -19,3 +19,21 @@ def test_it_has_a_rider():
     """A dragon has a rider with a name."""
     elise = Dragon('Elise', 'Green', 'Lessa')
     assert elise.get_rider() == 'Lessa'
+
+
+def test_dragons_are_born_hungry():
+    """When a dragon is born it is hungry."""
+    gretchen = Dragon('Gretchen', 'Yellow', 'Lacey')
+    assert gretchen.is_hungry() is True
+
+
+def test_dragons_eat_a_lot():
+    """If a dragon eats more than 3 times, it will no longer be hungry."""
+    regina = Dragon('Regina', 'Red', 'Rachel')
+    assert regina.is_hungry() is True
+    regina.eat()
+    regina.eat()
+    regina.eat()
+    assert regina.is_hungry() is True
+    regina.eat()
+    assert regina.is_hungry() is False
