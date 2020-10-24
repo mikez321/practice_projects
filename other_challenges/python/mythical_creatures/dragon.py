bcaf0e26f3bec1aad070047f41a5af5ec708efe6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-23 20:49:53 -0600

    Create getters and remove public attributes

--- other_challenges/python/mythical_creatures/dragon.py
+++ other_challenges/python/mythical_creatures/dragon.py
@@ -4,6 +4,20 @@
 class Dragon():
     """Methods for dragon object."""
 
-    def __init__(self, name):
+    def __init__(self, name, color, rider):
         """Create a dragon object with a name."""
         self.name = name
+        self.color = color
+        self.rider = rider
+
+    def get_name(self):
+        """Getter method for returning dragon's name."""
+        return self.name
+
+    def get_color(self):
+        """Getter method for returning dragon's color."""
+        return self.color
+
+    def get_rider(self):
+        """Getter method for returning dragon's rider name."""
+        return self.rider
