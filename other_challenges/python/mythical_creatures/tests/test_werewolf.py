205262f522f9df81c6b1a03050152c1f5e619e29
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-24 11:06:57 -0600

    Update tests to reflect changes to werewolf model

--- other_challenges/python/mythical_creatures/tests/test_werewolf.py
+++ other_challenges/python/mythical_creatures/tests/test_werewolf.py
@@ -4,41 +4,33 @@ from werewolf import Werewolf
 
 def test_it_exists():
     """Test that a werewolf object is created."""
-    werewolf = Werewolf()
+    werewolf = Werewolf('David', 'London')
     assert type(werewolf) == Werewolf
 
 
 def test_it_has_attributes():
     """Test that a werewolf has a name and location."""
-    werewolf = Werewolf()
-    werewolf.name = 'David'
-    werewolf.location = 'London'
+    werewolf = Werewolf('David', 'London')
     assert werewolf.name == 'David'
     assert werewolf.location == 'London'
 
 
 def test_it_is_human_by_default():
     """Test that a werewolf starts off human."""
-    werewolf = Werewolf()
-    werewolf.name = 'David'
-    werewolf.location = 'London'
+    werewolf = Werewolf('David', 'London')
     assert werewolf.human is True
 
 
 def test_it_can_change_into_a_werewolf():
     """Test that the change method makes it a werewolf."""
-    werewolf = Werewolf()
-    werewolf.name = 'David'
-    werewolf.location = 'London'
+    werewolf = Werewolf('David', 'London')
     werewolf.change()
     assert werewolf.human is False
 
 
 def test_it_can_cange_back_into_human_form():
     """If a werewolf is in wolf form and changes it becomes human."""
-    werewolf = Werewolf()
-    werewolf.name = 'David'
-    werewolf.location = 'London'
+    werewolf = Werewolf('David', 'London')
     assert werewolf.human is True
     werewolf.change()
     assert werewolf.human is False
@@ -48,7 +40,7 @@ def test_it_can_cange_back_into_human_form():
 
 def test_it_is_hungry_when_it_becomes_a_werewolf():
     """Werewolves are hungry, humans are not."""
-    werewolf = Werewolf()
+    werewolf = Werewolf('David', 'London')
     assert werewolf.human is True
     assert werewolf.hungry is False
     werewolf.change()
