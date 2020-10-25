8de2db9c7d95e003c46057e06f3885d19e16c885
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-24 19:08:38 -0600

    Add Person and properties for them and new medusa methods

--- other_challenges/python/mythical_creatures/medusa.py
+++ other_challenges/python/mythical_creatures/medusa.py
@@ -2,10 +2,50 @@
 class Medusa(object):
     """Medusa object and methods."""
 
-    def __init__(self, name):
+    def __init__(self, name: str) -> object:
         """Initialize with hidden properties."""
         self._name = name
+        self._statues = []
 
-    def get_name(self):
+    def get_name(self) -> str:
         """Getter method for name prop."""
-        return self.name
+        return self._name
+
+    def statue_count(self) -> int:
+        """Return the number of statues in the collection."""
+        return len(self._statues)
+
+    def stare_at(self, victim):
+        """
+        Stone the person and add to statue collection.
+
+        :param victim: Person object taken in as arg.  The person taken into
+            this method will be added to the medusa's collection of statues
+            and its property of being stoned will be set to true.
+        :return: No return value is produced from this method.
+        """
+        victim.stoned = True
+        self._statues.append(victim)
+
+
+class Person(object):
+    """Badic person object to interact with Medusa."""
+
+    def __init__(self, name) -> object:
+        """Initialize a standard person object with a name."""
+        self._name = name
+        self._stoned = False
+
+    def get_stoned(self):
+        """Getter method for stoned status."""
+        return self._stoned
+
+    def set_stoned(self, value):
+        """Change the value of _stoned."""
+        self._stoned = value
+
+    stoned = property(get_stoned, set_stoned)
+
+    def is_stoned(self):
+        """Getter method for stoned status."""
+        return self._stoned
--- other_challenges/python/mythical_creatures/tests/test_medusa.py
+++ other_challenges/python/mythical_creatures/tests/test_medusa.py
@@ -1,6 +1,25 @@
 """Tests for medusa.py."""
 from medusa import Medusa
+from medusa import Person
+
 
 def test_it_exists():
+    """Ensure barb is a medusa object."""
     barb = Medusa('Barb')
     assert type(barb) == Medusa
+
+
+def test_it_has_a_name_and_no_statues_when_new():
+    """New medusas have a name and no statues."""
+    jane = Medusa('Jane')
+    assert jane.get_name() == 'Jane'
+    assert jane.statue_count() == 0
+
+
+def test_stare_at_victim():
+    """Stare at a victim adds victim to statue collection."""
+    anna = Medusa('Anna')
+    pete = Person('Pete')
+    assert pete.is_stoned() is False
+    anna.stare_at(pete)
+    assert pete.is_stoned() is True
