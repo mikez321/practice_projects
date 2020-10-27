c251d9ff32f4ae2df2de4b1d7f24feebb828b08a
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-27 13:22:50 -0600

    Set up unittesting for sortbits cassidoo challenge

---
+++ casidoo_challenges/sort_bits.py
@@ -0,0 +1,14 @@
+"""SortBits Cassidoo Challenge."""
+import unittest
+
+
+class SortBitsTest(unittest.TestCase):
+    """Testing for sortBits function."""
+
+    def test_it_sorts_numbers_based_on_their_binary_representation(self):
+        """Sort ints in ascending order by number of 1s in their binary rep."""
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
