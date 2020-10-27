315039cb97ef712b9177887a656f4768eee326b8
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-27 13:28:16 -0600

    Set up function docstring and test

--- casidoo_challenges/sort_bits.py
+++ casidoo_challenges/sort_bits.py
@@ -2,12 +2,26 @@
 import unittest
 
 
+def sort_bits(nums: list) -> list:
+    """
+    Sort the integers by the number of 1’s in their binary representation.
+
+    :param nums: A list of ints.
+    :return: A sorted list of the ints in nums, sorted in assending order by
+        the number of 1s in their binary representation.
+    :example:
+        sort_bits([0,1,2,3,4,5,6,7,8])
+        >>> [0,1,2,4,8,3,5,6,7]
+    """
+
+
 class SortBitsTest(unittest.TestCase):
     """Testing for sortBits function."""
 
     def test_it_sorts_numbers_based_on_their_binary_representation(self):
         """Sort ints in ascending order by number of 1s in their binary rep."""
-        self.assertEqual(1, 1)
+        self.assertEqual(sort_bits([0, 1, 2, 3, 4, 5, 6, 7, 8]),
+                         [0, 1, 2, 4, 8, 3, 5, 6, 7])
 
 
 if __name__ == '__main__':
