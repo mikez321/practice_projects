abde2e9a38f31306114b659cafb5d68d469426ec
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-28 13:21:30 -0600

    Set up testing for 3 words function test

---
+++ other_challenges/python/three_words.py
@@ -0,0 +1,16 @@
+"""Three words challenge from checkio.org."""
+import unittest
+
+
+class ThreeWordsTest(unittest.TestCase):
+    """Testing for three words function."""
+
+    def test_it_returns_true_if_there_are_3_words_in_succession(self):
+        """Return true if 3 words are in succession."""
+        # assertEqual(three_words('Hello World hello'), True)
+        # assertEqual(three_words('He is 123 man'), False)
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
