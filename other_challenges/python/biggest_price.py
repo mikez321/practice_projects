f33ca386188941ae50f41b2db94ef0f93c342684
Mike Hernandez <hernandezmichael321@gmail.com>
2020-10-29 16:21:39 -0600

    Set up testing for biger price function.

---
+++ other_challenges/python/biggest_price.py
@@ -0,0 +1,14 @@
+"""Bigger Price challenge from checkio.org."""
+import unittest
+
+
+class PriceTest(unittest.TestCase):
+    """Testing for bigger_price function."""
+
+    def test_it_can_return_a_single_highest_price(self):
+        """It can return the highest price of all items."""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
