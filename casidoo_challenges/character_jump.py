e40107b0b29d941dea323c1ff6cd1c89c18f4f39
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-03 15:56:35 -0700

    Set up testing for character jump challenge

---
+++ casidoo_challenges/character_jump.py
@@ -0,0 +1,12 @@
+"""Character Jump Cassidoo Challenge."""
+import unittest
+
+
+class CharacterJumpTest(unittest.TestCase):
+    """Tests for char_jump function."""
+
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
