5b7e206689bdd9e597df2c22dd10b1cdf0231e6a
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-03 16:37:45 -0700

    Write docstring for days_diff function

--- other_challenges/python/days_between.py
+++ other_challenges/python/days_between.py
@@ -1,3 +1,13 @@
 """Days between challenge from checkio."""
-def days_diff(date1, date2):
-    return("Lets get this function started!")
+
+
+def days_diff(date1: tuple, date2: tuple) -> int:
+    """
+    Return the days between two dates.
+
+    :param date1: A date passed in as a tuple of ints in the format of
+        (year, month, day).
+    :param date2: This is another tuple of ints in the same format as date1.
+    :return:  The difference in number of days between the two dates is
+        returned.  It will always be a positive value or zero.
+    """
