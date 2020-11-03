4df7716224f97ba0d2b8a6e84d122c0be779aba9
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-03 16:00:14 -0700

    Set up test cases for char_jump function

--- casidoo_challenges/character_jump.py
+++ casidoo_challenges/character_jump.py
@@ -5,7 +5,14 @@ import unittest
 class CharacterJumpTest(unittest.TestCase):
     """Tests for char_jump function."""
 
-    pass
+    def test_it_passes_if_it_doesnt_hit_an_object(self):
+        """True if it hits no obstacles."""
+        self.assertEqual(character_jump(3, [0, 1, 0, 0, 0, 1, 0]))
+
+    @unittest.skip('Pass first then fail.')
+    def test_it_fails_if_it_hits_an_object(self):
+        """False if it hits an obstacle."""
+        self.assertEqual(character_jump(4, [0, 1, 1, 0, 1, 0, 0, 0, 0]))
 
 
 if __name__ == '__main__':
