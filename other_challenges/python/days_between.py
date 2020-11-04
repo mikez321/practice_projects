af59d1b62d858d8fdd2b5e83a227eaf3180f04c2
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-03 18:10:58 -0700

    Write function that passes only a few days apart

--- other_challenges/python/days_between.py
+++ other_challenges/python/days_between.py
@@ -11,3 +11,24 @@ def days_diff(date1: tuple, date2: tuple) -> int:
     :return:  The difference in number of days between the two dates is
         returned.  It will always be a positive value or zero.
     """
+    months = {
+        1: 31,
+        2: 28,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31,
+    }
+    y1, m1, d1 = date1
+    days1 = y1 * 365 + months[m1] + d1
+
+    y2, m2, d2 = date2
+    days2 = y2 * 365 + months[m2] + d2
+
+    return abs(days1 - days2)
