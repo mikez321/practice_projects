d671463f42061678265036e6d752bf38d50f1a85
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-03 18:37:32 -0700

    Create test to cover days between multiple years

--- other_challenges/python/test_days_between.py
+++ other_challenges/python/test_days_between.py
@@ -10,3 +10,8 @@ def test_it_can_return_a_few_days_apart():
 def test_it_can_return_a_larger_date_range():
     """Return the difference in two far apart days."""
     assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+
+
+def test_it_can_return_days_over_many_years():
+    """Return he difference in two days years apart."""
+    assert days_diff((2014, 1, 1), (2012, 1, 1)) == 730
