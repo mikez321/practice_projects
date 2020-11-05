8ab1ab06d16e7331c00a282d4d8f51238a5d63a3
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-05 13:51:07 -0700

    Use python datetime module to calculate dates

--- other_challenges/python/days_between.py
+++ other_challenges/python/days_between.py
@@ -1,4 +1,5 @@
 """Days between challenge from checkio."""
+from datetime import datetime
 
 
 def days_diff(date1: tuple, date2: tuple) -> int:
@@ -11,40 +12,7 @@ def days_diff(date1: tuple, date2: tuple) -> int:
     :return:  The difference in number of days between the two dates is
         returned.  It will always be a positive value or zero.
     """
-    months = {
-        1: 31,
-        2: 28,
-        3: 31,
-        4: 30,
-        5: 31,
-        6: 30,
-        7: 31,
-        8: 31,
-        9: 30,
-        10: 31,
-        11: 30,
-        12: 31,
-    }
-
     y1, m1, d1 = date1
-    elapsed_days = 0
-    for year in range(1, y1 + 1):
-        if year % 4 == 0:
-            elapsed_days += 1
-
-    for month in months.keys():
-        if m1 > month:
-            elapsed_days += months[month]
-    days1 = y1 * 365 + elapsed_days + d1
-
     y2, m2, d2 = date2
-    elapsed_days = 0
-    for year in range(1, y2 + 1):
-        if year % 4 == 0:
-            elapsed_days += 1
-    for month in months.keys():
-        if m2 > month:
-            elapsed_days += months[month]
-    days2 = y2 * 365 + elapsed_days + d2
-
-    return abs(days1 - days2)
+    delta = datetime(y1, m1, d1) - datetime(y2, m2, d2)
+    return(abs(delta.days))
--- other_challenges/python/test_days_between.py
+++ other_challenges/python/test_days_between.py
@@ -13,15 +13,16 @@ def test_it_can_return_a_larger_date_range():
 
 
 def test_it_can_return_days_over_many_years():
-    """Return he difference in two days years apart."""
-    assert days_diff((2014, 1, 1), (2012, 1, 1)) == 730
+    """Return the difference in two days years apart."""
+    assert days_diff((2018, 1, 1), (2020, 1, 1)) == 730
 
 
 def test_it_can_take_leap_years_into_account():
     """Every 4 years is a leap year."""
     assert days_diff((1, 1, 1), (2, 1, 1,)) == 365
-    assert days_diff((3, 1, 1), (4, 1, 1,)) == 366
-#
-# def test_it_can_return_days_from_the_start_to_the_end_of_time():
-#     """Start from 1 and end at 9999."""
-#     assert days_diff((1, 1, 1), (9999, 12, 31)) == 3652058
+    assert days_diff((2020, 1, 1), (2021, 1, 1,)) == 366
+
+
+def test_it_can_return_days_from_the_start_to_the_end_of_time():
+    """Start from 1 and end at 9999."""
+    assert days_diff((1, 1, 1), (9999, 12, 31)) == 3652058
