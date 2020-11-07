9f82d553cfd86d1f660d83406a779059d8539427
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-07 14:25:59 -0700

    Add ability to add responses to choices

--- other_challenges/python/decisions/decision.py
+++ other_challenges/python/decisions/decision.py
@@ -7,3 +7,27 @@ class Decision(object):
     def __init__(self):
         """Decisions have nothing to decide on at first."""
         self.choices = {}
+
+    def add_choice(self, choice: str) -> None:
+        """
+        Add something to be decided upon.
+
+        :param choice: A string of anything you want to be deciding on
+        :return: This does not return anything but does update self.choices
+        """
+        self.choices[choice] = []
+
+    def add_response(self, choice: str, res: str) -> None:
+        """
+        Add a response to a given choice.
+
+        :param choice: A string of the choice to get a response.  This choice
+            shold exist in self.choices.
+        :param res: A string of positive feedback about that choice.  The
+            response will be added to the list of responses for that choice
+            and eventually will be added to determine which choice has the most
+            positive responses.
+        :return: None is returned from this function.  It only updaes
+            self.choices.
+        """
+        self.choices[choice].append(res)
--- other_challenges/python/decisions/test_decision.py
+++ other_challenges/python/decisions/test_decision.py
@@ -12,3 +12,28 @@ def test_it_has_nothing_to_decide_about_when_new():
     """New decisions have nothing to decide about when new."""
     decision = Decision()
     assert decision.choices == {}
+
+
+def test_choices_can_be_added():
+    """Choices can be added to decide upon."""
+    decision = Decision()
+    decision.add_choice("Truck")
+    assert decision.choices == {"Truck": []}
+    decision.add_choice("Van")
+    assert decision.choices == {"Truck": [], "Van": []}
+
+
+def test_responses_can_be_added_to_choices():
+    """A choice can have responses added to it."""
+    decision = Decision()
+    decision.add_choice('Truck')
+    decision.add_choice('Van')
+    decision.add_response('Truck', 'It goes offroad real nice')
+    decision.add_response('Van', 'You can sleep in it')
+
+    expected = {
+        'Truck': ['It goes offroad real nice'],
+        'Van': ['You can sleep in it'],
+    }
+
+    assert decision.choices == expected
