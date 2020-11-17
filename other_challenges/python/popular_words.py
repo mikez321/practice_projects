8be6a1bcfccfcebd047bad6329fb70dd8c88721e
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-17 14:34:37 -0700

    Set up testing for popular_words function

---
+++ other_challenges/python/popular_words.py
@@ -0,0 +1,14 @@
+"""Popular words challenge from checkio."""
+import unittest
+
+
+class PopularWordsTest(unittest.TestCase):
+    """Testing for popular_words function."""
+
+    def test_it_returns_words_by_popularity(self):
+        """Return words and their popularity from a string."""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
