ae7e24c03da73984b0f47003c3a473d31883c3cb
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-18 10:15:47 -0700

    Set up test env for left right challenge from check.io

---
+++ other_challenges/python/left_right.py
@@ -0,0 +1,14 @@
+"""Left Right challenge from check.io."""
+import unittest
+
+
+class LeftRightTest(unittest.TestCase):
+    """Testing for leftright function."""
+
+    def test_it_messes_up_instructions(self):
+        """First test for leftright function."""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
