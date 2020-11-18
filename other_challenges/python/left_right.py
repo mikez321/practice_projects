965509b0ad23ebf1bc37c674a04bb52f2f8726d3
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-18 10:24:10 -0700

    Write docstring and set up function test

--- other_challenges/python/left_right.py
+++ other_challenges/python/left_right.py
@@ -2,12 +2,27 @@
 import unittest
 
 
+def left_join(phrases: tuple) -> str:
+    """
+    Return a single string from a tuple of strings.
+
+    :param prhases:  A tuple of strings
+    :return: All strings returned as a single, comma separated string.
+        However, in the return, any instance of 'right' should be replaced
+        with 'left' even if it is in the middle of a word.
+    :example:
+        left_join(("bright aright", "ok"))
+        >>> "bleft aleft,ok"
+    """
+
+
 class LeftRightTest(unittest.TestCase):
     """Testing for leftright function."""
 
     def test_it_messes_up_instructions(self):
         """First test for leftright function."""
-        pass
+        self.assertEqual(
+            left_join("left", "right", "left", "stop"), "left,left,left,stop")
 
 
 if __name__ == '__main__':
