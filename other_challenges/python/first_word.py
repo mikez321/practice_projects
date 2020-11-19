37a4bdc2c3032c0e412e97819471e5dcae3e39df
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-18 21:25:59 -0700

    Set up unittest

---
+++ other_challenges/python/first_word.py
@@ -0,0 +1,14 @@
+"""First word challenge from checkio."""
+import unittest
+
+
+class FirstWordTest(unittest.TestCase):
+    """Testing for first word function."""
+
+    def test_it_returns_the_first_word(self):
+        """Function returns the first word of a given string."""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
