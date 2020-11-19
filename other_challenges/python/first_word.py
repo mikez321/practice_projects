25673f0d570e0dbc7a5d0e464a4fb4f53844ad63
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-18 21:29:47 -0700

    Set up function and write docstring

--- other_challenges/python/first_word.py
+++ other_challenges/python/first_word.py
@@ -2,12 +2,23 @@
 import unittest
 
 
+def first_word(text: str) -> str:
+    """
+    Return the first word of a given string.
+
+    :param text: Any string of text.
+    :return: The first word in that string of text is returned. If there is an
+        apostrophe and it is part of the word it should be included.  Erroneous
+        punctuation before or after a word should be removed.
+    """
+
+
 class FirstWordTest(unittest.TestCase):
     """Testing for first word function."""
 
     def test_it_returns_the_first_word(self):
         """Function returns the first word of a given string."""
-        pass
+        self.assertEqual(first_word("Hello world"), "Hello")
 
 
 if __name__ == '__main__':
