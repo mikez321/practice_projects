1aff77937a78242e429705646cc3795aab94f260
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-20 09:15:03 -0700

    Solve special pairs challenge

--- casidoo_challenges/special_pairs.rb
+++ casidoo_challenges/special_pairs.rb
@@ -2,6 +2,30 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 
-class SpecialPairsTest < Minitest::Test
+def special_pairs(nums)
+  pairs = []
+  special_pairs = []
+  indexes = (0.. nums.length - 1)
 
+  indexes.each do |ind1|
+    indexes.each do |ind2|
+      ind1 < ind2 ? pairs << [ind1, ind2] : next
+    end
+  end
+
+  pairs.each do |pair|
+    nums[pair.first] == nums[pair.last] ? special_pairs << pair : next
+  end
+
+  special_pairs.length
+end
+
+class SpecialPairsTest < Minitest::Test
+  def test_it_returns_a_special_pair
+    assert_equal special_pairs([1, 2, 3, 1]), 1
+  end
+
+  def test_it_returns_all_special_pairs
+    assert_equal special_pairs([1, 2, 3, 1, 1, 3]), 4
+  end
 end
