f7e597d1207aa0446100d9b64c3b2017195d2eec
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-21 11:00:44 -0700

    Set up testing for split_list challenge

---
+++ other_challenges/python/split_list.py
@@ -0,0 +1,14 @@
+"""Split list challenge from checkio."""
+import unittest
+
+
+class SplitListTest(unittest.TestCase):
+    """Testing for split_list function."""
+
+    def test_it_splits_a_list(self):
+        """It can split a single list into two lists."""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
