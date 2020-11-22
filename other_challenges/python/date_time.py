b5f1705136b61fcda4ece64a01b4a56f92109291
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-22 13:20:01 -0700

    Get basic functionality working

--- other_challenges/python/date_time.py
+++ other_challenges/python/date_time.py
@@ -2,12 +2,12 @@
 import unittest
 
 
-def date_time(date: str) -> str:
+def date_time(date_time: str) -> str:
     """
     Convert number time to human readable time.
 
-    :param date: A string of numbers representing a date and time in the format
-        DD.MM.YYYY HH:MM  (ex: '01.01.2000 12:30' representing
+    :param date_time: A string of numbers representing a date and time in the
+        format DD.MM.YYYY HH:MM  (ex: '01.01.2000 12:30' representing
         1 January 2000, 12:30 PM)  *Time will be 24hour format.
     :return: A human readable version of the date and time will be returned in
         the format of '1 January 2000 year 1 hour 59 minutes'  *Note that time
@@ -15,6 +15,50 @@ def date_time(date: str) -> str:
         minute, but 'hours' and 'minutes' for times like
         '2 hours and 25 minutes.'
     """
+    months = {
+        '01': 'January',
+        '02': 'February',
+        '03': 'March',
+        '04': 'April',
+        '05': 'May',
+        '06': 'June',
+        '07': 'July',
+        '08': 'August',
+        '09': 'September',
+        '10': 'October',
+        '11': 'November',
+        '12': 'December',
+    }
+
+    date = date_time.split()[0]
+    day = date.split('.')[0]
+    month = months[date.split('.')[1]]
+    year = date.split('.')[-1]
+
+    if day[0] == '0':
+        day = day[1:]
+
+    time = date_time.split()[-1]
+    minutes = time.split(':')[-1]
+    hours = time.split(':')[0]
+
+    if minutes[0] == '0':
+        minutes = minutes[1:]
+
+    if hours[0] == '0':
+        hours = hours[1:]
+
+    if minutes == '1':
+        minutes = minutes + ' minute'
+    else:
+        minutes = minutes + ' minutes'
+
+    if hours == '1':
+        hours = hours + ' hour'
+    else:
+        hours = hours + ' hours'
+
+    return day + ' ' + month + ' ' + year + ' year ' + hours + ' ' + minutes
 
 
 class DateTimeTest(unittest.TestCase):
