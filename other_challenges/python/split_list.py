b6285983eda81dd7a7cd6c9e3888d5279b359a05
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-22 12:41:13 -0700

    Solve spit list for one and no items in list

--- other_challenges/python/split_list.py
+++ other_challenges/python/split_list.py
@@ -13,15 +13,26 @@ def split_list(items: list) -> list:
         If the original list has an odd number of items, the first list will
         contain one more item than the second.
     """
-    split = (len(items) // 2) * -1
-    first_half = items[:split]
-    second_half = items[len(items) - 1:split - 1:-1]
+    if len(items) == 1:
+        return [items, []]
+    else:
+        split = (len(items) // 2) * -1
+        first_half = items[:split]
+        second_half = items[len(items) - 1:split - 1:-1]
 
-    return [first_half, second_half[::-1]]
+        return [first_half, second_half[::-1]]
 
 
 class SplitListTest(unittest.TestCase):
     """Testing for split_list function."""
+
+    def test_it_can_split_nothing(self):
+        """If an empty list is provided an empty list is returned."""
+        self.assertEqual(split_list([]), [[], []])
+
+    def test_it_can_split_a_list_of_one_item(self):
+        """It can split a single item in a list."""
+        assert split_list([1]) == [[1], []]
 
     def test_it_splits_a_list(self):
         """It can split a single list into two lists."""
