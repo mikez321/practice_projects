3ddf90843663d3fde979513fd2ed39ab2a72fea7
Mike Hernandez <hernandezmichael321@gmail.com>
2020-12-19 09:54:11 -0700

    Set up basic test for sunangle challenge from checkio

---
+++ other_challenges/python/sunangle.py
@@ -0,0 +1,10 @@
+import unittest
+
+class SunAngleTest(unittest.TestCase):
+    """Testing for angle of sun at a given time."""
+
+    def test_zero_degrees_at_sunrise(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
