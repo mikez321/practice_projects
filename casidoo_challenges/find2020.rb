7a6c4691b4914ad01b0f69b167f463769aac10c0
Mike Hernandez <hernandezmichael321@gmail.com>
2020-12-30 09:32:54 -0700

    Fix function to take in string not array

--- casidoo_challenges/find2020.rb
+++ casidoo_challenges/find2020.rb
@@ -1,8 +1,20 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 
-def find2020(number)
-  -1 if number.length < 4
+def find2020(number, index = 0, result = -1)
+  if number.length < 4
+    result
+  else
+    first_four = number[0, 4]
+    if first_four == '2020'
+      result = index
+    else
+      number = number[1, number.length - 1]
+      index += 1
+      find2020(number, index, result)
+    end
+  end
+  result
 end
 
 class FindTwentyTwentyTest < Minitest::Test
@@ -11,6 +23,12 @@ class FindTwentyTwentyTest < Minitest::Test
   end
 
   def test_no_2020_returns_minus_one
-    assert_equal(find2020([1]), -1)
+    assert_equal(find2020('1'), -1)
+    assert_equal(find2020('222'), -1)
+    assert_equal(find2020('22200'), -1)
   end
+
+  # def test_it_can_find_a_2020
+  #   assert_equal(find2020('2002022020002'), 4)
+  # end
 end
