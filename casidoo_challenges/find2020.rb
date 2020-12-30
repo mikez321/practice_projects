703e787ac40a98eb3a887368ec3433228936fdd7
Mike Hernandez <hernandezmichael321@gmail.com>
2020-12-30 08:25:31 -0700

    Solve for cases where number is less than 4 chars in length

--- casidoo_challenges/find2020.rb
+++ casidoo_challenges/find2020.rb
@@ -1,8 +1,16 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 
+def find2020(number)
+  -1 if number.length < 4
+end
+
 class FindTwentyTwentyTest < Minitest::Test
   def test_it_works
     assert_equal(1, 1)
+  end
+
+  def test_no_2020_returns_minus_one
+    assert_equal(find2020([1]), -1)
   end
 end
