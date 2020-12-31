0c5d93ccef8dfabcc8b724f3bdbbeea79892712c
Mike Hernandez <hernandezmichael321@gmail.com>
2020-12-30 20:46:32 -0700

    Set up unittest

---
+++ other_challenges/python/lambdas.py
@@ -0,0 +1,14 @@
+"""Lambda exploration."""
+import unittest
+
+
+class LambdaTest(unittest.TestCase):
+    """Tests go here."""
+
+    def test_lambda_function(self):
+        """Making sure testing env is working."""
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
