a6ae0dcadd4cfd4178af953a8a095016f4cc9712
Mike Hernandez <hernandezmichael321@gmail.com>
2020-12-30 20:56:38 -0700

    Use lambda inline to pass tests with no code written

--- other_challenges/python/lambdas.py
+++ other_challenges/python/lambdas.py
@@ -2,16 +2,13 @@
 import unittest
 
 
-power_fun = lambda a, b: a ** b
-
-
 class LambdaTest(unittest.TestCase):
     """Tests go here."""
 
     def test_lambda_function(self):
         """Test number to a power lambda function."""
-        self.assertEqual(power_fun(2, 3), 8)
-        self.assertEqual(power_fun(4, 5), 1024)
+        self.assertEqual((lambda a, b: a**b)(2, 3), 8)
+        self.assertEqual((lambda a, b: a**b)(4, 5), 1024)
 
 
 if __name__ == "__main__":
