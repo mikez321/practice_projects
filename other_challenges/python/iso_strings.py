5fafc8015f2be5ad427242f5075dca0a714323e8
Mike Hernandez <hernandezmichael321@gmail.com>
2020-12-30 21:08:37 -0700

    Write docstring for isometric_strings

--- other_challenges/python/iso_strings.py
+++ other_challenges/python/iso_strings.py
@@ -2,12 +2,31 @@
 import unittest
 
 
+def isometric_strings(str1: string, str2: string) -> bool:
+    """
+    Return true if string1 and string2 are isometric.
+    :example:
+        add and egg are isometric because you can set a=e and d=g and they will
+        spell out the two words respectively.
+        Conversely, foo and bar are not isometric, because you can not swap out
+        any one letter for another and make new words.
+        isometric_strings('add', 'egg')
+        >>> True
+        isometric_strings('foo', 'bar')
+        >>> False
+    """
+
+
 class IsoStringsTest(unittest.TestCase):
     """Testing for iso_strings function."""
 
     def test_basic_true(self):
         """Set up testing."""
         self.assertEqual(1, 1)
+
+    def test_egg(self):
+        """Egg and Add are isometric strings."""
+        self.assertEqual(isometric_strings('add', 'egg'), True)
 
 
 if __name__ == '__main__':
