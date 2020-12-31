4c6db3e5772d3817422f51ec0633a3eabaf75f01
Mike Hernandez <hernandezmichael321@gmail.com>
2020-12-30 20:50:57 -0700

    Write a little lambda function

--- other_challenges/python/lambdas.py
+++ other_challenges/python/lambdas.py
@@ -2,12 +2,16 @@
 import unittest
 
 
+power_fun = lambda a, b: a ** b
+
+
 class LambdaTest(unittest.TestCase):
     """Tests go here."""
 
     def test_lambda_function(self):
-        """Making sure testing env is working."""
-        self.assertEqual(1, 1)
+        """Test number to a power lambda function."""
+        self.assertEqual(power_fun(2, 3), 8)
+        self.assertEqual(power_fun(4, 5), 1024)
 
 
 if __name__ == "__main__":
