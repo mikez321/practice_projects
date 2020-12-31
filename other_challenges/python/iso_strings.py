1e15dd9f4f0ec20f233c4f27c9d17fbe22ac0cd6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-12-30 21:03:09 -0700

    Set up testing for isostrings function

---
+++ other_challenges/python/iso_strings.py
@@ -0,0 +1,14 @@
+"""Surjection Strings challenge from checkio."""
+import unittest
+
+
+class IsoStringsTest(unittest.TestCase):
+    """Testing for iso_strings function."""
+
+    def test_basic_true(self):
+        """Set up testing."""
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
