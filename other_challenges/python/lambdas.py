c2083a3e02f41ac84edfc00f8790537a4d53a50f
Mike Hernandez <hernandezmichael321@gmail.com>
2020-12-30 20:57:25 -0700

    Add one more test case with an everyday power

--- other_challenges/python/lambdas.py
+++ other_challenges/python/lambdas.py
@@ -7,6 +7,7 @@ class LambdaTest(unittest.TestCase):
 
     def test_lambda_function(self):
         """Test number to a power lambda function."""
+        self.assertEqual((lambda a, b: a**b)(2, 2), 4)
         self.assertEqual((lambda a, b: a**b)(2, 3), 8)
         self.assertEqual((lambda a, b: a**b)(4, 5), 1024)
 
