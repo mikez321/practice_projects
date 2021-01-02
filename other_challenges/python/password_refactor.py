9d8a4a0fb90b80c9a87abc0c73b428f36666a973
Mike Hernandez <hernandezmichael321@gmail.com>
2021-01-01 20:18:44 -0700

    Set up test for password check challenge

---
+++ other_challenges/python/password_refactor.py
@@ -0,0 +1,18 @@
+"""5 part password challenge from checkio."""
+import unittest
+# This was a 5 part password verification challenge from Checkio.org.
+# I completed it, but here I will just copy/paste my code and start from the
+# end.  I hope to be able to refactor it using tools I've learned over the
+# past few days... lambdas and regex.
+
+
+class PasswordCheckTest(unittest.TestCase):
+    """Testing for checkio password checker."""
+
+    def test_setup(self):
+        """Check that test is set up properly."""
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
